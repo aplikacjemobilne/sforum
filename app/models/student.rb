71675403ec586c8c468eb06d73e5d324f5c4f23c
Pawel Ksieniewicz <pawel@ksienie.com>
2017-11-21 19:16:24 +0100

    1.2.5 Zabezpieczenie użytkowników hasłem

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,10 @@ class Student < ApplicationRecord
   has_and_belongs_to_many :courses, :join_table => :students_courses
   has_many :topics
   has_many :posts
+
+  has_secure_password
+
+  validates :name, presence: true, uniqueness: true, length: { in: 3..50 }
+  validates :index, presence: true, length: { is: 6 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
