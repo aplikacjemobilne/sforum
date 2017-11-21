04a6f3fc29d995767d549567b9f7958cc08bdbc9
Pawel Ksieniewicz <pawel@ksienie.com>
2017-11-21 19:49:26 +0100

    1.3.1 Wypełnianie bazy danych przykładowymi wpisami

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,61 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+students = Student.create(
+  [
+    {
+      index: '123456',
+      name: 'Zenon',
+      password: 'haslo123'
+    },
+    {
+      index: '654321',
+      name: 'Marian',
+      password: 'haslo123'
+    }
+  ]
+)
+
+courses = Course.create(
+  [
+    {
+      code: '1234',
+      name: 'Aplikacje mobilne',
+      description: 'Opis kursu'
+    },
+    {
+      code: '4321',
+      name: 'Inny kurs',
+      description: 'Opis innego kursu'
+    }
+  ]
+)
+
+topics = Topic.create(
+  [
+    {
+      title: 'Co z tą Polską?',
+      student: students.first,
+      course: courses.first
+    },
+    {
+      title: 'Jak żyć?',
+      student: students.last,
+      course: courses.first
+    }
+  ]
+)
+
+posts = Post.create(
+  [
+    {
+      body: 'Sam nie wiem',
+      student: students.first,
+      topic: topics.first
+    },
+    {
+      body: 'Ja też',
+      student: students.last,
+      topic: topics.first
+    }
+  ]
+)
+
+students.first.courses = courses
